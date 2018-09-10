edb6ef0ccec7199a579c6ffcd135f72d9b6c1882
Donovan <SimonDon@berkeleyprep.org>
2018-09-10 12:00:15 -0400

    Good!

--- Homework1.java
+++ Homework1.java
@@ -1,5 +1,5 @@
 /*
- * First and Last Names
+ * Donovan Simonton
  */
 
 import java.awt.Rectangle;
@@ -10,8 +10,8 @@ public class Homework1 {
 	 */
 	public static int problem1() {
 		int mystery = 1;
-		mystery = mystery + 1;
-		int mystery = 1 - 2 * mystery;
+		mystery = mystery + 2;
+		mystery = -1 * mystery;
 		return mystery;
 	}
 
@@ -19,25 +19,30 @@ public class Homework1 {
 	 * height of h
 	 */
 	public static Rectangle problem2(int w, int h) {
+		Rectangle rect = new Rectangle(0, 0, w, h);
+		return rect;
 
 	}
 
 	/* Declare an integer, assign it a value of 5, then return it
 	 */
 	public static int problem3() {
-
+		int five = 5;
+		return five;
 	}
 
 	/* Declare and return a String containing the phrase "Computer Science"
 	 */
 	public static String problem4() {
-
+		String str = new String("Computer Science");
+		return str;
 	}
 
 	/* Add 5 to num and return the new value
 	 */
 	public static int problem5(int num) {
-
+			num = num + 5;
+			return num;
 	}
 
 }
